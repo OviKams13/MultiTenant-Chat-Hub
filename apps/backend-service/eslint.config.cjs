9d74bf1374f38a92be189409e6f23d6c37af068d
OviKams13 <okambembo13@gmail.com>
2026-03-14 18:24:45 +0200

    add shared public contracts package and backend contract integration

--- apps/backend-service/eslint.config.cjs
+++ apps/backend-service/eslint.config.cjs
@@ -1,23 +1,3 @@
-const tsParser = require('@typescript-eslint/parser');
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-const prettierPlugin = require('eslint-plugin-prettier');
-const { typescriptRules } = require('../../packages/eslint-config/node.cjs');
+const { nodeConfig } = require('@mth/eslint-config/node');
 
-module.exports = [
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2020,
-      sourceType: 'module'
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      prettier: prettierPlugin
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-      ...typescriptRules
-    }
-  }
-];
+module.exports = nodeConfig;
